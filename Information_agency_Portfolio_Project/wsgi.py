48f4c22a76ec1539e3aee9f0c12c0f687ad6e42a
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2025-12-06 14:54:03 +0100

    Initialize empty django project with name Information agency Portfolio Project

---
+++ Information_agency_Portfolio_Project/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for Information_agency_Portfolio_Project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "Information_agency_Portfolio_Project.settings"
+)
+
+application = get_wsgi_application()
