7b03d75492ebf5753217e207dda5746bb6f45359
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2025-12-06 15:09:01 +0100

    changed admin panel, add custom fields for Redactor model

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from newspaper.models import Topic, Newspaper, Redactor
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Your experience", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    search_fields = "title",
+    list_filter = "published",
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    search_fields = "name",
